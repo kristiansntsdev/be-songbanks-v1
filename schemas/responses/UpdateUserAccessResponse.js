8c2be8fb4e245dad339522bb977609a455b55484
kristiansnts <epafroditus.kristian@gmail.com>
2025-08-01 21:33:17 +0700

    feat/migrate: update user to match with database and update api endpoint

--- schemas/responses/UpdateUserAccessResponse.js
+++ schemas/responses/UpdateUserAccessResponse.js
@@ -1,6 +1,6 @@
 export default {
   200: {
-    description: "User access status updated successfully",
+    description: "User access status or role updated successfully",
     content: {
       "application/json": {
         schema: {
@@ -12,24 +12,43 @@ export default {
             },
             message: {
               type: "string",
-              example: "User status changed to active",
+              example: "User status updated to active successfully",
             },
             data: {
               type: "object",
               properties: {
                 id: {
-                  type: "string",
+                  type: "integer",
                   description: "User ID",
-                  example: "user123",
+                  example: 17,
+                },
+                nama: {
+                  type: "string",
+                  description: "User full name",
+                  example: "Melody Charis Frynikel Lakoy",
+                },
+                username: {
+                  type: "string",
+                  description: "User email/username",
+                  example: "melody.19197@gmail.com",
                 },
                 status: {
                   type: "string",
                   description: "Updated user status",
                   example: "active",
+                  enum: ["active", "suspend", "request", "pending"],
+                },
+                role: {
+                  type: "string",
+                  description: "User role",
+                  example: "member",
+                  enum: ["guest", "member", "admin"],
                 },
               },
+              required: ["id", "nama", "username", "status", "role"],
             },
           },
+          required: ["code", "message", "data"],
         },
       },
     },
